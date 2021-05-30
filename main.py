31c02c11ec51e7d5b247cec15789b515e6706567
4yonatan4 <4yonatan4@gmail.com>
2021-05-30 10:55:08 +0300

    First Commit. No code yet, just import some libraries.

---
+++ main.py
@@ -0,0 +1,11 @@
+# Shilo Lieopold ?????, Yonatan Gat
+
+import hashlib
+from cryptography.fernet import Fernet
+
+# Generate symmetric key
+# key = Fernet.generate_key()
+
+if __name__ == '__main__':
+    print('PyCharm')
+
